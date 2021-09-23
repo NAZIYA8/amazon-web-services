7a2c8ead489633e071abe6422e580043950ab8bb
NAZIYA8 <syeda.naziya896@gmail.com>
2021-09-24 02:57:47 +0530

    [Naziya]Add: Logger file to perform the logger operations

---
+++ S3/LoggerFormat.py
@@ -0,0 +1,6 @@
+import logging
+logger = logging
+logger.basicConfig(filename='s3.log', level=logging.INFO,
+                    format='%(asctime)s:%(funcName)s:%(levelname)s:%(message)s')
+logger.basicConfig(filename='s3.log', level=logging.ERROR,
+                    format='%(asctime)s:%(funcName)s:%(levelname)s:%(lineno)d')
